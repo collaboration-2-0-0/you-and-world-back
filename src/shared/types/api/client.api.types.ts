e46dc5c7e96921f292b0586da1d975b9e753d295
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2026-03-05 12:51:11 +0200

    fix: client api

--- src/shared/types/api/client.api.types.ts
+++ src/shared/types/api/client.api.types.ts
@@ -4,8 +4,6 @@ export type TAccountLogin_tg = {
 export type TAccountSignup_tg = {
   initData: string;
 };
-export type TAdminNetGetResponse = {
-};
 export type TBotMessage = {
   chatId: number;
   netId?: number;
